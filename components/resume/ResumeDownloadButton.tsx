50b7122fd9f357561ad326939b8f6285ecfeb93f
thisisxjj <763801840@qq.com>
2024-07-14 18:14:24 +0800

    - 添加简历下载功能

--- components/resume/ResumeDownloadButton.tsx
+++ components/resume/ResumeDownloadButton.tsx
@@ -2,14 +2,21 @@
 
 import React from 'react'
 import { Button } from '../ui/button'
-import Link from 'next/link'
 import { FiDownload } from 'react-icons/fi'
 import { useMessages } from 'next-intl'
 import { updateResumeDownloadCount } from '@/lib/update.action'
+import { downloadPDF } from '@/lib/utils'
 
 const ResumeDownloadButton = ({ locale }: { locale: string }) => {
   const messages = useMessages()
   const homeMessages = messages.Home as Record<string, string>
+
+  const handleDownloadClick = async (e: any) => {
+    downloadPDF(
+      `https://static.thisisxjj.com/pdf/resume_${locale}.pdf`,
+      homeMessages['resumeFilename']
+    )
+  }
 
   return (
     <Button
@@ -19,10 +26,10 @@ const ResumeDownloadButton = ({ locale }: { locale: string }) => {
       className="uppercase flex items-center gap-2"
       onClick={() => updateResumeDownloadCount(locale)}
     >
-      <Link href="#">
+      <div onClick={handleDownloadClick}>
         <span>{homeMessages['downloadResume']}</span>
         <FiDownload className="text-xl" />
-      </Link>
+      </div>
     </Button>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,46 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// Function to download the PDF
+export async function downloadPDF(
+  url: string,
+  fileName: string = 'Filename.pdf'
+) {
+  try {
+    // Fetch the PDF file
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/pdf',
+      },
+    })
+
+    // Check if the request was successful
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    // Convert the response to a blob
+    const blob = await response.blob()
+
+    // Create a link element
+    const link = document.createElement('a')
+    link.href = window.URL.createObjectURL(blob)
+    link.download = fileName // You can specify the filename here
+
+    // Append the link to the body
+    document.body.appendChild(link)
+
+    // Programmatically click the link to trigger the download
+    link.click()
+
+    // Remove the link from the document
+    document.body.removeChild(link)
+  } catch (error) {
+    console.error('There was an error downloading the PDF:', error)
+  }
 }
